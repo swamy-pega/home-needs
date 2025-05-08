6e5f841bfcb9ed123afc9559cdb2f1b4bec6c9b7
swamy-pega <swamy.p@hotmail.com>
2025-05-08 01:13:42 -0400

    up

--- src/components/TopHandyManList.jsx
+++ src/components/TopHandyManList.jsx
@@ -9,10 +9,10 @@ const TopHandyManList = () => {
 
 <h1 className='text-3xl font-medium'>Top Handyman List</h1>
 <p className='sm:w-1/3 text-center text-sm' >Here are the top handyman list</p>
-<div className='w-50% grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+<div className='w-50% grid grid-cols-[1fr_1fr_1fr_1fr_1fr] gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
 {HandymanList.slice(0,10).map( (items,index)=>
 (
-    <div className='border grid grid-cols-auto border-blue-200 rounded-xl p-4 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+    <div className='border grid grid-cols-[1fr] border-blue-200 rounded-xl p-4 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
 <img className='bg-blue-5 w-44 'src={items.image} alt=""/>
 <div className='p-4'> 
     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
